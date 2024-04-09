42b84fb9659f707bc142d7299bb13d026c38866e
User <user@Users-MacBook-Pro.local>
2024-04-09 19:32:10 +0200

    first commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${inter.className} bg-[#88C1EE]`}>{children}</body>
     </html>
   );
 }
